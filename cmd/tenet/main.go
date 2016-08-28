a993fe131f0a82dfb9da4c8e17c05da95c4934d3
Robert Scott Dionne <robertsdionne@gmail.com>
2016-08-27 22:59:41 -0400

    Remove HTTP package.

--- cmd/tenet/main.go
+++ cmd/tenet/main.go
@@ -3,25 +3,10 @@ package main
 import (
 	"flag"
 	"github.com/robertsdionne/tenet/grpc"
-	"github.com/robertsdionne/tenet/http"
-	"github.com/robertsdionne/tenet/prot"
 	"log"
 )
 
-var protocol = prot.GRPC
-
 func main() {
-	flag.Var(&protocol, "protocol", "The server's protocol.")
 	flag.Parse()
-
-	switch protocol {
-	case prot.GRPC:
-		log.Fatalln(grpc.ListenAndServe())
-	case prot.HTTP:
-		log.Fatalln(http.ListenAndServe())
-	case prot.HTTP2:
-		log.Fatalln("HTTP2 is not yet implemented.")
-	default:
-		log.Fatalln("Unknown protocol.")
-	}
+	log.Fatalln(grpc.ListenAndServe())
 }
