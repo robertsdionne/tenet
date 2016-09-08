a61bc0284a81b8287ab0ad9f6d862e2f181b1ad3
Robert Scott Dionne <robertsdionne@gmail.com>
2016-09-08 12:01:02 -0400

    Add hyperbolic_tangent_test.

---
+++ ten/hyperbolic_tangent_test.go
@@ -0,0 +1,34 @@
+package ten
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHyperbolicTangentGradient(t *testing.T) {
+	x, dy := New(3, 3), New(3, 3)
+
+	x.Data = []float64{
+		0, 1, 2,
+		1, 2, 3,
+		2, 3, 4,
+	}
+
+	dy.Data = []float64{
+		1, 1, 1,
+		1, 1, 1,
+		1, 1, 1,
+	}
+
+	y := HyperbolicTangent(x)
+	dx := HyperbolicTangentGradient(dy, y)
+
+	hyperbolicTangent := func(tensors ...Tensor) (y Tensor) {
+		y = DualHyperbolicTangent(tensors[0])
+		return
+	}
+
+	gradients := TestGradients(hyperbolicTangent, &x)
+
+	assert.InDeltaSlice(t, gradients[&x].Data, dx.Data, 1e-10)
+}
--- ten/hyperoblic_tangent.go
+++ ten/hyperoblic_tangent.go
@@ -14,6 +14,18 @@ func HyperbolicTangent(x Tensor) (y Tensor) {
 	return
 }
 
+func DualHyperbolicTangent(x Tensor) (y Tensor) {
+	y = NewLike(x)
+
+	for i := range x.Real().Data {
+		y.Real().Data[i] = math.Tanh(x.Real().Data[i])
+		cosh := math.Cosh(x.Real().Data[i])
+		y.Dual().Data[i] = x.Dual().Data[i] / cosh / cosh
+	}
+
+	return
+}
+
 func HyperbolicTangentGradient(dy, y Tensor) (dx Tensor) {
 	dx = NewLike(y)
 
