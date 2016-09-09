de7bab2dad62d148e1212e35a0ddb019ab421be1
Robert Scott Dionne <robertsdionne@gmail.com>
2016-09-09 10:33:49 -0400

    Softmax model uses logistic activation for now.

--- mod/mnist/softmax_test.go
+++ mod/mnist/softmax_test.go
@@ -1,49 +1,49 @@
 package mnist
 
-// import (
-// 	"github.com/robertsdionne/tenet/ten"
-// 	"github.com/stretchr/testify/assert"
-// 	"testing"
-// )
-//
-// func TestSoftmaxGradient(t *testing.T) {
-// 	W, b, x, dy := ten.New(3, 3), ten.New(3, 1), ten.New(3, 1), ten.New(3, 1)
-//
-// 	W.Data = []float64{
-// 		0, 1, 2,
-// 		1, 2, 3,
-// 		2, 3, 4,
-// 	}
-//
-// 	b.Data = []float64{
-// 		0,
-// 		1,
-// 		2,
-// 	}
-//
-// 	x.Data = []float64{
-// 		0,
-// 		1,
-// 		2,
-// 	}
-//
-// 	dy.Data = []float64{
-// 		1,
-// 		0,
-// 		0,
-// 	}
-//
-// 	_, _, y := softmaxForward(W, b, x)
-// 	dw, db, dx, _, _ := softmaxGradient(W, b, x, y, dy)
-//
-// 	forward := func(tensors ...ten.Tensor) (y ten.Tensor) {
-// 		_, _, y = dualSoftmaxForward(tensors[0], tensors[1], tensors[2])
-// 		return
-// 	}
-//
-// 	gradients := ten.TestGradients(forward, dy, &W, &b, &x)
-//
-// 	assert.Equal(t, gradients[&W], dw)
-// 	assert.Equal(t, gradients[&b], db)
-// 	assert.Equal(t, gradients[&x], dx)
-// }
+import (
+	"github.com/robertsdionne/tenet/ten"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSoftmaxGradient(t *testing.T) {
+	W, b, x, dy := ten.New(3, 3), ten.New(3, 1), ten.New(3, 1), ten.New(3, 1)
+
+	W.Data = []float64{
+		0, 1, 2,
+		1, 2, 3,
+		2, 3, 4,
+	}
+
+	b.Data = []float64{
+		0,
+		1,
+		2,
+	}
+
+	x.Data = []float64{
+		0,
+		1,
+		2,
+	}
+
+	dy.Data = []float64{
+		1,
+		0,
+		0,
+	}
+
+	_, _, y := softmaxForward(W, b, x)
+	dw, db, dx, _, _ := softmaxGradient(W, b, x, y, dy)
+
+	forward := func(tensors ...ten.Tensor) (y ten.Tensor) {
+		_, _, y = dualSoftmaxForward(tensors[0], tensors[1], tensors[2])
+		return
+	}
+
+	gradients := ten.TestGradients(forward, dy, &W, &b, &x)
+
+	assert.InDeltaSlice(t, gradients[&W].Data, dw.Data, 1e-10)
+	assert.InDeltaSlice(t, gradients[&b].Data, db.Data, 1e-10)
+	assert.InDeltaSlice(t, gradients[&x].Data, dx.Data, 1e-10)
+}
